097d864eff89c18301cc2a5d9f5369ac292879e4
Lxcilferrr <emer.zuniga@duocuc.cl>
2023-09-22 15:51:25 -0300

    uso camara

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'listar',
+    redirectTo: 'usocamara',
     pathMatch: 'full'
   },
   {
--- src/app/pages/usocamara/usocamara.page.ts
+++ src/app/pages/usocamara/usocamara.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-usocamara',
@@ -14,6 +15,22 @@ export class UsocamaraPage implements OnInit {
   ngOnInit() {
   }
 
-  takePicture() {}
+ takePicture = async () => {
+    const image2 = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+  
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    //var imageUrl = image.webPath;
+    this.image = image2.dataUrl;
+    // Can be set to the src of an image now
+    //imageElement.src = imageUrl;
+  };
 
 }
